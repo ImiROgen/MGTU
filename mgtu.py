ae9b2ab659d1df57b5a094a2a7b0f4edb35d42ef
Artem <91467084+ImiROgen@users.noreply.github.com>
2021-10-29 15:56:50 +0300

    Add files via upload
    Изменения согласно результатам проверки:
    - добавлены 2 пустые строки после импортирования
    - инициализация переменных и вызовы функций разделены
    - в именах переменных применены только буквы нижнего регистра и нижнее подчеркивание

--- mgtu.py
+++ mgtu.py
@@ -1,24 +1,27 @@
 import pygame as pg
+
+
 pg.init()
-gameRun = True
-SIZE = H, W = 500, 500
+H, W = 500, 500
 FPS = 60
-JumpStep = 10
+jumpstep = 10
 WHITE = (255, 255, 255)
 GREEN = (0, 200, 200)
-C_rad = 40
+c_rad = 40
 clock = pg.time.Clock()
-JCount_const = 5
+jcount_const = 5
+
+pg.init()
+sc = pg.display.set_mode((H, W))
 pg.display.set_caption('Игра по заданию МГТУ')
-sc = pg.display.set_mode(SIZE)
 sc.fill(WHITE)
 
 
 def check_close():
     for event in pg.event.get():
         if event.type == pg.QUIT:
+            print('Game Over')
             pg.quit()
-            gameRun = False
 
 
 class Circle:
@@ -28,35 +31,35 @@ class Circle:
         self.y = y
         self.rad = rad
         self.color = color
-        self.JCount = 10
-        self.Space_pressed = False
+        self.jcount = 10
+        self.space_pressed = False
 
     def drawing(self):
         pg.draw.circle(sc, self.color, (self.x, self.y), self.rad)
 
     def move(self):
         keys = pg.key.get_pressed()
-        if keys[pg.K_LEFT] and self.x > C_rad:
-            self.x -= JumpStep
-        elif keys[pg.K_RIGHT] and self.x < W - C_rad:
-            self.x += JumpStep
-        if not(self.Space_pressed):
-            if keys[pg.K_UP] and self.y > self.JCount + (C_rad * 2):
-                self.y -= JumpStep
-            if keys[pg.K_DOWN] and self.y < H - C_rad - self.JCount:
-                self.y += JumpStep
+        if keys[pg.K_LEFT] and self.x > c_rad:
+            self.x -= jumpstep
+        elif keys[pg.K_RIGHT] and self.x < W - c_rad:
+            self.x += jumpstep
+        if not(self.space_pressed):
+            if keys[pg.K_UP] and self.y > self.jcount + (c_rad * 2):
+                self.y -= jumpstep
+            if keys[pg.K_DOWN] and self.y < H - c_rad - self.jcount:
+                self.y += jumpstep
             if keys[pg.K_SPACE]:
-                self.Space_pressed = True
+                self.space_pressed = True
         else:
-            if self.JCount >= -10:
-                self.y -= int(self.JCount * abs(self.JCount) * 0.5)
-                self.JCount -= 1
+            if self.jcount >= -10:
+                self.y -= int(self.jcount * abs(self.jcount) * 0.5)
+                self.jcount -= 1
             else:
-                self.Space_pressed = False
-                self.JCount = 10
+                self.space_pressed = False
+                self.jcount = 10
 
 
-Gameboy = Circle(W // 2, H // 2, C_rad, GREEN)
+Gameboy = Circle(W // 2, H // 2, c_rad, GREEN)
 
 
 def redraw():
@@ -65,11 +68,8 @@ def redraw():
     pg.display.update()
 
 
-while gameRun:
+while True:
     check_close()
     Gameboy.move()
     redraw()
     clock.tick(FPS)
-
-
-print('Game Over')
